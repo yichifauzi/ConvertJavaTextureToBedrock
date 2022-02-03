895de3dc6b109bb3ade0238388c1749c6d2a9143
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-02-03 11:22:52 -0600

    Update update_gh-pages.sh

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -3,9 +3,9 @@ cd ../webapp-gh-pages
 
 rm -rf $(ls -a -I '.' -I '..' -I '.git')
 
-docker pull ozelot379/convert-minecraft-java-texture-to-bedrock-webapp
+docker pull ModifiedCommand/convert-minecraft-java-texture-to-bedrock-webapp
 
-docker run -d --name convert-minecraft-java-texture-to-bedrock-webapp ozelot379/convert-minecraft-java-texture-to-bedrock-webapp
+docker run -d --name convert-minecraft-java-texture-to-bedrock-webapp ModifiedCommand/convert-minecraft-java-texture-to-bedrock-webapp
 
 docker cp convert-minecraft-java-texture-to-bedrock-webapp:/usr/local/apache2/htdocs/ ./
 
